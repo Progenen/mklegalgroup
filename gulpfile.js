d7b3d4c289a9dbd4dee74e8115ba9b2b2f808585
John Doe <stanislav_2010@icloud.com>
2021-02-22 11:16:20 +0500

    Gulp Update

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 gulp.task('server', function() {
 
@@ -33,7 +35,7 @@ gulp.task('watch', function() {
     gulp.watch("src/*.html", gulp.parallel('html'));
     gulp.watch('src/JS/**/*', gulp.parallel('scripts'));
     gulp.watch('src/images/**/*', gulp.parallel('img'));
-    // gulp.watch('src/fonts/**/*', gulp.parallel('fonts'));
+    gulp.watch('src/fonts/**/*', gulp.parallel('fonts'));
 })
 
 gulp.task('html', function() {
@@ -42,7 +44,9 @@ gulp.task('html', function() {
 })
 
 gulp.task('scripts', function() {
-    return gulp.src('src/JS/**/*')
+    return gulp.src('src/JS/*.js')
+        .pipe(uglify())
+        .pipe(concat('all.min.js'))
         .pipe(gulp.dest('dist/JS/'));
 })
 
@@ -52,9 +56,9 @@ gulp.task('img', function() {
         .pipe(gulp.dest('dist/images/'));
 })
 
-// gulp.task('fonts', function(){
-//     return gulp.src('src/fonts/**/*')
-//         .pipe(gulp.dest('dist/fonts/'));
-// })
+gulp.task('fonts', function(){
+    return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts/'));
+})
 
-gulp.task('default', gulp.parallel('watch', 'html', 'scripts', 'img', 'server', 'styles'));
+gulp.task('default', gulp.parallel('watch', 'html', 'scripts', 'img', 'server', 'styles', 'fonts'));
