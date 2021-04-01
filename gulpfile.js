936e16a9799e032845c249f14496e1ed79e63b4d
John Doe <stanislav_2010@icloud.com>
2021-04-01 16:19:30 +0500

    Template v1.02

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,81 @@
-
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
+const { src, dest, watch, series, parallel } = require('gulp');
+const browserSync  = require('browser-sync');
+const sass         = require('gulp-sass');
+const cleanCSS     = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
-const imagemin = require('gulp-imagemin');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const rename       = require("gulp-rename");
+const imagemin     = require('gulp-imagemin');
+const concat       = require('gulp-concat');
+const uglify       = require('gulp-uglify');
+const newer        = require('gulp-newer');
+const del          = require('del');
+const fileinclude  = require('gulp-file-include');
 
-gulp.task('server', function() {
-
-    browserSync({
+function browsersync () {
+    browserSync.init({
         server: {
             baseDir: "dist"
         }
     });
+}
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
-
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+function style () {
+    return src('src/sass/**/*.+(scss|sass)')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
+        .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true}))
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("dist/css"))
-        .pipe(browserSync.stream());
-});
+        .pipe(dest("dist/css"))
+        .pipe(browserSync.stream())
+}
 
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-    gulp.watch("src/*.html", gulp.parallel('html'));
-    gulp.watch('src/JS/**/*', gulp.parallel('scripts'));
-    gulp.watch('src/images/**/*', gulp.parallel('img'));
-    gulp.watch('src/fonts/**/*', gulp.parallel('fonts'));
-})
+function html () {
+    return src('src/**/*.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(dest('dist/'))
+}
 
-gulp.task('html', function() {
-    return gulp.src('src/*.html')
-        .pipe(gulp.dest('dist/'));
-})
+function scripts () {
+    return src('src/JS/**/*.js')
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(dest('dist/JS/'))
+}
 
-gulp.task('scripts', function() {
-    return gulp.src('src/JS/*.js')
-        .pipe(uglify())
-        .pipe(concat('all.min.js'))
-        .pipe(gulp.dest('dist/JS/'));
-})
+function startWatch () {
+    watch(['src/sass/**/*.+(scss|sass)'], style);
+    watch(['src/JS/**/*.js', '!src/**/*.min.js'], scripts);
+    watch(['src/*.html'], html);
+    watch(['src/*.html']).on('change', browserSync.reload);
+    watch(['src/images/**/*'], images);
+    watch(['src/fonts/**/*'], fonts);
+}
 
-gulp.task('img', function() {
-    return gulp.src('src/images/**/*')
+function images () {
+    return src('src/images/**/*')
+        .pipe(newer('dist/images'))
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/images/'));
-})
+        .pipe(dest('dist/images/'))
+}
 
-gulp.task('fonts', function(){
-    return gulp.src('src/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts/'));
-})
+function cleanImg () {
+    return del('dist/images/**/*', {force: true})
+}
 
-gulp.task('default', gulp.parallel('watch', 'html', 'scripts', 'img', 'server', 'styles', 'fonts'));
+function fonts () {
+    return src('src/fonts/**/*')
+        .pipe(dest('dist/fonts/'))
+}
+
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.style = style;
+exports.html = html;
+exports.cleanImg = cleanImg;
+exports.fonts = fonts;
+
+exports.default = parallel(style, html, scripts, fonts, images, browsersync, startWatch);
+
