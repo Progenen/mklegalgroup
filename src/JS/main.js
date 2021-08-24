d5f32ba57c7b67d9a086bb212902eefede092335
John Doe <stanislav_2010@icloud.com>
2021-08-24 12:20:50 +0500

    Start

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     const menu = document.querySelector('.burger-menu');
-    const menuBody = document.querySelector('.menu__body');
+    const menuBody = document.querySelectorAll('.header__menu');
+    const mobileMenu = document.querySelector('.mobile__menu');
 
     if (menu != null) {
-        menu.addEventListener('click', function() {
-            const group = [menu, menuBody]; 
-            group.forEach(element => {
-                element.classList.toggle('.active')
+
+        if (screen.width < 1200) {
+            menuBody.forEach(element => {
+                mobileMenu.append(element);
             });
-            body.classList.toggle('.lock');
+        }
+    
+        menu.addEventListener('click', function() {
+            const group = [menu, mobileMenu]; 
+            group.forEach(element => {
+                element.classList.toggle('active');
+            });
+            document.body.classList.toggle('lock');
         });
     }
 });
